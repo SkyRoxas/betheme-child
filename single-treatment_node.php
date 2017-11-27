b8179c73dc842504b3c2dbf3823931e815a7c74a
skyroxas <skyroxas1521@gmail.com>
2017-11-28 00:57:27 +0800

    single treatment page

--- single-treatment_node.php
+++ single-treatment_node.php
@@ -11,58 +11,77 @@ get_header();
 ?>
 
 <!-- #Content -->
-<div id="Content">
+<div id="Content" class="p-lg-5" style="box-sizing:border-box;">
 	<div class="content_wrapper clearfix">
 
 		<!-- .sections_group -->
 		<div class="sections_group">
-			<?php
 
-				if( get_post_meta( get_the_ID(), 'mfn-post-template', true ) == 'builder' ){
+			<?php if( get_post_meta( get_the_ID(), 'mfn-post-template', true ) == 'builder' ):?>
 
-					// Template | Builder -----------------------------------------------
+				<?php
+				$single_post_nav = array(
+					'hide-sticky'	=> false,
+					'in-same-term'	=> false,
+				);
 
-					$single_post_nav = array(
-						'hide-sticky'	=> false,
-						'in-same-term'	=> false,
-					);
-
-					$opts_single_post_nav = mfn_opts_get( 'prev-next-nav' );
-					if( isset( $opts_single_post_nav['hide-sticky'] ) ){
-						$single_post_nav['hide-sticky'] = true;
-					}
-
-					// single post navigation | sticky
-					if( ! $single_post_nav['hide-sticky'] ){
-						if( isset( $opts_single_post_nav['in-same-term'] ) ){
-							$single_post_nav['in-same-term'] = true;
-						}
-
-						$post_prev = get_adjacent_post( $single_post_nav['in-same-term'], '', true );
-						$post_next = get_adjacent_post( $single_post_nav['in-same-term'], '', false );
-
-						echo mfn_post_navigation_sticky( $post_prev, 'prev', 'icon-left-open-big' );
-						echo mfn_post_navigation_sticky( $post_next, 'next', 'icon-right-open-big' );
-					}
-
-
-					while( have_posts() ){
-						the_post();							// Post Loop
-						mfn_builder_print( get_the_ID() );	// Content Builder & WordPress Editor Content
-					}
-
-				} else {
-
-					// Template | Default -----------------------------------------------
-
-					while( have_posts() ){
-						the_post();
-						get_template_part( 'includes/content', 'single' );
-					}
-
+				$opts_single_post_nav = mfn_opts_get( 'prev-next-nav' );
+				if( isset( $opts_single_post_nav['hide-sticky'] ) ){
+					$single_post_nav['hide-sticky'] = true;
 				}
 
-			?>
+				// single post navigation | sticky
+				if( ! $single_post_nav['hide-sticky'] ){
+					if( isset( $opts_single_post_nav['in-same-term'] ) ){
+						$single_post_nav['in-same-term'] = true;
+					}
+
+					$post_prev = get_adjacent_post( $single_post_nav['in-same-term'], '', true );
+					$post_next = get_adjacent_post( $single_post_nav['in-same-term'], '', false );
+
+					echo mfn_post_navigation_sticky( $post_prev, 'prev', 'icon-left-open-big' );
+					echo mfn_post_navigation_sticky( $post_next, 'next', 'icon-right-open-big' );
+				}
+
+
+				while( have_posts() ){
+					the_post();							// Post Loop
+					mfn_builder_print( get_the_ID() );	// Content Builder & WordPress Editor Content
+				}
+				?>
+
+			<?php else: ?>
+
+				<!-- Template | Builder -->
+
+				<?php while (have_posts()) : the_post(); ?>
+
+					<?php
+					function get_field_image($field){
+						echo '<img src="'.get_field($field)['url'].'" alt="'.get_field($field)['alt'].'">';
+					}
+					?>
+
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-3 col-12 mb-5">
+								<?php get_field_image('icon_node'); ?>
+							</div>
+							<div class="col">
+								<h3 class="single-title mb-4"><?php the_title();?></h3>
+								<?php the_content(); ?>
+							</div>
+						</div>
+					</div>
+
+
+
+
+					<?php //get_template_part( 'includes/content', 'single' ); ?>
+
+				<?php endwhile; ?>
+
+			<?php endif; ?>
 		</div>
 
 		<!-- .four-columns - sidebar -->
@@ -71,6 +90,4 @@ get_header();
 	</div>
 </div>
 
-<?php get_footer();
-
-// Omit Closing PHP Tags
+<?php get_footer(); ?>
